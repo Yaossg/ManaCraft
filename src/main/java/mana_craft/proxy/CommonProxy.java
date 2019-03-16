e14171dd1ee49df767d7258637b1d6ca1033de46
Yaossg <2640712744@qq.com>
2019-03-16 19:21:48 +0800

    new sausage core api

--- src/main/java/mana_craft/proxy/CommonProxy.java
+++ src/main/java/mana_craft/proxy/CommonProxy.java
@@ -1,6 +1,8 @@
 package mana_craft.proxy;
 
 import mana_craft.ManaCraft;
+import mana_craft.api.registry.MBFuel;
+import mana_craft.api.registry.MPRecipe;
 import mana_craft.block.BlockManaHead;
 import mana_craft.config.ManaCraftConfig;
 import mana_craft.entity.ManaCraftVillagers;
@@ -16,23 +18,34 @@ import mana_craft.tile.TileManaProducer;
 import mana_craft.world.biome.ManaCraftBiomes;
 import mana_craft.world.gen.ManaCraftWorldGens;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import sausage_core.api.core.ienum.IEnumGUIHandler;
+import sausage_core.api.registry.SCFRecipeManager;
 import sausage_core.api.util.common.SausageUtils;
-import sausage_core.api.util.plugin.PluginPrimer;
+import sausage_core.api.util.plugin.PluginLoader;
 
+import static mana_craft.api.registry.ManaCraftRegistries.MB_FUELS;
+import static mana_craft.api.registry.ManaCraftRegistries.MP_RECIPES;
 import static mana_craft.block.ManaCraftBlocks.*;
 import static mana_craft.item.ManaCraftItems.*;
 import static net.minecraftforge.oredict.OreDictionary.registerOre;
 
 public class CommonProxy {
-    static final PluginPrimer pluginPrimer = new PluginPrimer();
+    static final PluginLoader pluginPrimer = new PluginLoader(ManaCraft.MODID);
     public void preInit(FMLPreInitializationEvent event) {
         IEnumGUIHandler.register(ManaCraft.instance, ManaCraftGUIs.values());
-        ManaCraftRecipes.init(event);
+        ResourceLocation mp_recipe = new ResourceLocation(ManaCraft.MODID, "mp_recipe");
+        SCFRecipeManager.registerType(mp_recipe, MPRecipe::parse);
+        ResourceLocation mb_fuel = new ResourceLocation(ManaCraft.MODID, "mb_fuel");
+        SCFRecipeManager.registerType(mb_fuel, MBFuel::parse);
+        SausageUtils.getPath(ManaCraft.class, "/assets/mana_craft/defaults/").ifPresent(root -> {
+            SCFRecipeManager.registerDefault(mp_recipe, root.resolve("recipes"));
+            SCFRecipeManager.registerDefault(mb_fuel, root.resolve("fuels"));
+        });
         ManaCraftPotions.preInit();
         ManaCraftVillagers.preInit();
         if(ManaCraftConfig.ticPlugin)
@@ -79,7 +92,8 @@ public class CommonProxy {
     }
 
     public void postInit(FMLPostInitializationEvent event) {
-        ManaCraftRecipes.loadAll();
+        MP_RECIPES.registerAll(SCFRecipeManager.recipesOf(new ResourceLocation(ManaCraft.MODID, "mp_recipe")));
+        MB_FUELS.registerAll(SCFRecipeManager.recipesOf(new ResourceLocation(ManaCraft.MODID, "mb_fuel")));
         if(ManaCraftConfig.grassMana)
             MinecraftForge.addGrassSeed(new ItemStack(mana), 1);
     }
--- src/main/java/mana_craft/recipe/ManaCraftRecipes.java
+++ src/main/java/mana_craft/recipe/ManaCraftRecipes.java
@@ -1,110 +1,12 @@
 package mana_craft.recipe;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import mana_craft.ManaCraft;
-import mana_craft.api.registry.MBFuel;
-import mana_craft.api.registry.MPRecipe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.JsonUtils;
-import net.minecraftforge.common.crafting.JsonContext;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
-import org.apache.commons.io.FilenameUtils;
-import sausage_core.api.util.common.SausageUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
-import java.util.stream.Collectors;
-
-import static mana_craft.api.registry.ManaCraftRegistries.MB_FUELS;
-import static mana_craft.api.registry.ManaCraftRegistries.MP_RECIPES;
 import static mana_craft.block.ManaCraftBlocks.*;
 import static mana_craft.item.ManaCraftItems.*;
 import static net.minecraftforge.fml.common.registry.GameRegistry.addSmelting;
 
 public class ManaCraftRecipes {
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-    private static final List<Path> recipePaths = new ArrayList<>();
-    private static final List<Path> fuelPaths = new ArrayList<>();
-    public static void init(FMLPreInitializationEvent event) {
-
-        SausageUtils.getPath(ManaCraft.class, "/assets/mana_craft/machines/").ifPresent(root -> {
-            recipePaths.add(root.resolve("recipes"));
-            fuelPaths.add(root.resolve("fuels"));
-        });
-        try {
-            Path config = event.getModConfigurationDirectory().toPath().resolve("mana_craft_machines");
-            if(!Files.exists(config))
-                Files.createDirectories(config);
-            Path recipes = config.resolve("recipes");
-            if(!Files.exists(recipes))
-                Files.createDirectories(recipes);
-            recipePaths.add(recipes);
-            Path fuels = config.resolve("fuels");
-            if(!Files.exists(fuels))
-                Files.createDirectories(fuels);
-            fuelPaths.add(fuels);
-        } catch (IOException e) {
-            ManaCraft.logger.info("Failed to find directories, so no recipes or fuels load from any file", e);
-            recipePaths.clear();
-            fuelPaths.clear();
-        }
-    }
-
-    public static void loadAll() {
-        int recipes = MP_RECIPES.view().size();
-        recipePaths.forEach(path -> loadEntries(walk(path), MPRecipe::parse, MP_RECIPES::register));
-        ManaCraft.logger.info("loaded {} recipes from files", MP_RECIPES.view().size() - recipes);
-        ManaCraft.logger.info("loaded {} recipes in total", MP_RECIPES.view().size());
-        int fuels = MB_FUELS.view().size();
-        fuelPaths.forEach(path -> loadEntries(walk(path), MBFuel::parse, MB_FUELS::register));
-        ManaCraft.logger.info("loaded {} fuels from files", MB_FUELS.view().size() - fuels);
-        ManaCraft.logger.info("loaded {} fuels in total", MB_FUELS.view().size());
-    }
-
-    private static List<Path> walk(Path value) {
-        try {
-            return Files.walk(value)
-                    .filter(path -> "json".equals(
-                            FilenameUtils.getExtension(path.getFileName().toString())))
-                    .collect(Collectors.toList());
-        } catch (IOException e) {
-            return Collections.emptyList();
-        }
-    }
-
-    private static <T> void loadEntries(List<Path> paths, BiFunction<JsonContext, JsonObject, T> parser, Consumer<T> consumer) {
-        if(paths.isEmpty()) return;
-        JsonContext context = new JsonContext(ManaCraft.MODID);
-        for (Path path : paths) {
-            if("_constants.json".equals(path.getFileName().toString())) {
-                try (BufferedReader reader = Files.newBufferedReader(path)) {
-                    JsonObject[] json = JsonUtils.fromJson(GSON, reader, JsonObject[].class);
-                    ReflectionHelper.findMethod(JsonContext.class, "loadConstants", "loadConstants", JsonObject[].class).invoke(context, (Object) json);
-                } catch (Exception e) {
-                    ManaCraft.logger.error("Unexpected Exception: ", e);
-                }
-            }
-        }
-        paths.removeIf(path -> path.getFileName().toString().startsWith("_"));
-        paths.forEach(path -> {
-            try (BufferedReader reader = Files.newBufferedReader(path)) {
-                JsonObject json = JsonUtils.fromJson(GSON, reader, JsonObject.class);
-                consumer.accept(parser.apply(context, json));
-            } catch (Exception e) {
-                ManaCraft.logger.error("Unexpected Exception: ", e);
-            }
-        });
-    }
 
     public static void addSmelt() {
         addSmelting(orichalcum_dust, new ItemStack(orichalcum_ingot), 0.05f);
